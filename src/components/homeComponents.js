f4e61c6ecc7cebeba7bad20403def74653877f83
EricTsai <etsai@MacBook-Pro.attlocal.net>
2019-05-07 21:48:14 -0700

    Making gh-static branch

--- src/components/homeComponents.js
+++ src/components/homeComponents.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Navbar, Nav, NavDropdown, Form, FormControl, Button, Container, Jumbotron, Row, Col } from "react-bootstrap";
 import { RadialChart, Hint } from 'react-vis';
 import { AppConsumer } from './appProviderComponent';
+import { ResponsivePie } from '@nivo/pie'
 import "../style/style.css";
 
 class Home extends Component {
@@ -58,6 +59,41 @@ class Header extends Component {
     }
 }
 
+
+const data =
+    [
+        {
+            "id": "javascript",
+            "label": "javascript",
+            "value": 122,
+            "color": "hsl(345, 70%, 50%)"
+        },
+        {
+            "id": "go",
+            "label": "go",
+            "value": 202,
+            "color": "hsl(60, 70%, 50%)"
+        },
+        {
+            "id": "ruby",
+            "label": "ruby",
+            "value": 172,
+            "color": "hsl(285, 70%, 50%)"
+        },
+        {
+            "id": "elixir",
+            "label": "elixir",
+            "value": 219,
+            "color": "hsl(240, 70%, 50%)"
+        },
+        {
+            "id": "hack",
+            "label": "hack",
+            "value": 454,
+            "color": "hsl(118, 70%, 50%)"
+        }
+    ]
+
 class Summary extends Component {
 
     getTotalSpending() {
@@ -87,7 +123,7 @@ class Summary extends Component {
                         <Container>
                             <Row>
                                 <Col>
-                                    <RadialChart
+                                    {/*                                     <RadialChart
                                         className={'donut-chart-example'}
                                         innerRadius={100}
                                         radius={140}
@@ -105,7 +141,8 @@ class Summary extends Component {
                                         height={300}
                                         padAngle={0.04}
                                         showLabels={true}
-                                    />
+                                    /> */}
+                                    <MyResponsivePie data={data} />
                                 </Col>
                                 <Col>
                                     <Row>
@@ -137,6 +174,147 @@ class Summary extends Component {
         )
     }
 }
+
+
+// make sure parent container have a defined height when using
+// responsive component, otherwise height will be 0 and
+// no chart will be rendered.
+// website examples showcase many properties,
+// you'll often use just a few of them.
+const MyResponsivePie = ({ data /* see data tab */ }) => (
+    <ResponsivePie
+        data={data}
+        margin={{
+            "top": 40,
+            "right": 80,
+            "bottom": 80,
+            "left": 80
+        }}
+        innerRadius={0.5}
+        padAngle={0.7}
+        cornerRadius={3}
+        colors={{
+            "scheme": "nivo"
+        }}
+        borderWidth={1}
+        borderColor={{
+            "from": "color",
+            "modifiers": [
+                [
+                    "darker",
+                    0.2
+                ]
+            ]
+        }}
+        radialLabelsSkipAngle={10}
+        radialLabelsTextXOffset={6}
+        radialLabelsTextColor="#333333"
+        radialLabelsLinkOffset={0}
+        radialLabelsLinkDiagonalLength={16}
+        radialLabelsLinkHorizontalLength={24}
+        radialLabelsLinkStrokeWidth={1}
+        radialLabelsLinkColor={{
+            "from": "color"
+        }}
+        slicesLabelsSkipAngle={10}
+        slicesLabelsTextColor="#333333"
+        animate={true}
+        motionStiffness={90}
+        motionDamping={15}
+        defs={[
+            {
+                "id": "dots",
+                "type": "patternDots",
+                "background": "inherit",
+                "color": "rgba(255, 255, 255, 0.3)",
+                "size": 4,
+                "padding": 1,
+                "stagger": true
+            },
+            {
+                "id": "lines",
+                "type": "patternLines",
+                "background": "inherit",
+                "color": "rgba(255, 255, 255, 0.3)",
+                "rotation": -45,
+                "lineWidth": 6,
+                "spacing": 10
+            }
+        ]}
+        fill={[
+            {
+                "match": {
+                    "id": "ruby"
+                },
+                "id": "dots"
+            },
+            {
+                "match": {
+                    "id": "c"
+                },
+                "id": "dots"
+            },
+            {
+                "match": {
+                    "id": "go"
+                },
+                "id": "dots"
+            },
+            {
+                "match": {
+                    "id": "python"
+                },
+                "id": "dots"
+            },
+            {
+                "match": {
+                    "id": "scala"
+                },
+                "id": "lines"
+            },
+            {
+                "match": {
+                    "id": "lisp"
+                },
+                "id": "lines"
+            },
+            {
+                "match": {
+                    "id": "elixir"
+                },
+                "id": "lines"
+            },
+            {
+                "match": {
+                    "id": "javascript"
+                },
+                "id": "lines"
+            }
+        ]}
+        legends={[
+            {
+                "anchor": "bottom",
+                "direction": "row",
+                "translateY": 56,
+                "itemWidth": 100,
+                "itemHeight": 18,
+                "itemTextColor": "#999",
+                "symbolSize": 18,
+                "symbolShape": "circle",
+                "effects": [
+                    {
+                        "on": "hover",
+                        "style": {
+                            "itemTextColor": "#000"
+                        }
+                    }
+                ]
+            }
+        ]}
+    />
+)
+
+
 Summary.contextType = AppConsumer; // This part is important to access context values
 
 
