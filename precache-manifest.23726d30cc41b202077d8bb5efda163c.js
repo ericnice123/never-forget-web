f6098d9edc0a05cf71232822cbbd9333dbff3ac9
EricTsai <etsai@MacBook-Pro.attlocal.net>
2019-05-07 22:12:22 -0700

    Updates

---
+++ precache-manifest.23726d30cc41b202077d8bb5efda163c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb15b93f6341f04fc6d32a3826acffa3",
+    "url": "/never-forget-web/index.html"
+  },
+  {
+    "revision": "9330bcbdcdc6657d42b0",
+    "url": "/never-forget-web/static/css/2.090dce78.chunk.css"
+  },
+  {
+    "revision": "587020705b972a258502",
+    "url": "/never-forget-web/static/css/main.dd300c82.chunk.css"
+  },
+  {
+    "revision": "9330bcbdcdc6657d42b0",
+    "url": "/never-forget-web/static/js/2.8c632b59.chunk.js"
+  },
+  {
+    "revision": "587020705b972a258502",
+    "url": "/never-forget-web/static/js/main.35e9ffac.chunk.js"
+  },
+  {
+    "revision": "dbcd67461b399fb79b66",
+    "url": "/never-forget-web/static/js/runtime~main.71a2abf1.js"
+  }
+]);
